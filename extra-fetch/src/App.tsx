9506e7a4ac3e88451a5d340c7a1d65149629b0e7
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-01-22 13:46:21 +0100

    Added fetch

--- extra-fetch/src/App.tsx
+++ extra-fetch/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { IOmdbResponse } from "./models/IOmdbResponse";
+import { IMovie } from "./models/IMovie";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
+  const getMovies = async () => {
+    const response = await fetch("https://omdbapi.com/?apikey=416ed51a&s=star");
+    const data: IOmdbResponse = await response.json();
+
+    setMovies(data.Search);
+  };
 
   return (
     <>
+      <button onClick={getMovies}>Hämta filmer</button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {movies.map((movie) => {
+          return (
+            <div key={movie.imdbID}>
+              <h3>{movie.Title}</h3>
+              <img src={movie.Poster} alt={movie.Title} />
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
