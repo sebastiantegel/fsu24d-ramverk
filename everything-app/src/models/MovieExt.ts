ac067b95436b08aa8d8f337eefd07cc4ffe0c626
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-01-30 11:41:21 +0100

    Added explanation

--- everything-app/src/models/MovieExt.ts
+++ everything-app/src/models/MovieExt.ts
@@ -16,3 +16,6 @@ export type MovieExt = Movie & {
 
 // const a: MovieExt = { Title: "", Poster: "", imdbID: "", Plot: "", Director: "", Actors: ""}
 // const b: Movie = { Title: "", Poster: "", imdbID: "" }
+
+// class används när vi behöver skapa egna objekt i vår applikation.
+// type/interface används när vi hämtar objekt från api:er.
